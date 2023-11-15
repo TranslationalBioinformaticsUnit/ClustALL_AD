091f11e1d43a6e351cbb160d906281393fae21e7
Sara Palomino <58259926+spalominoe@users.noreply.github.com>
2023-11-15 15:05:44 +0100

    Update 00_Functions.R

--- R_code/00_Functions.R
+++ R_code/00_Functions.R
@@ -1,1 +1,26 @@
+######## Load required packages and functions ########
 
+
+### Required packages
+
+x <- c("dplyr", "ggplot2", "cluster", "mclust", "clValid", "modeest", "mice",
+       "FactoMineR", "MASS", "corrplot", "clusteval", "fpc", "haven", "naniar",
+      "reshape2", "circlize", "labelled",  "Rtsne")
+
+lapply(x, require, character.only = TRUE)
+
+###  Required Functions
+
+# > PCA derived information
+
+generatePCA_derived <- function(data,variability,maxvar)
+{
+  pca_go = PCA(data, graph = FALSE,ncp=min(c(maxvar,ncol(data))))
+  variab <- pca_go$eig[,3]
+  whichgo <- min(which(variab>50))
+  varigo <- min(c(whichgo,maxvar))
+  res <- pca_go$ind$coord[,c(1:varigo)]
+  res
+}
+
+# > 
